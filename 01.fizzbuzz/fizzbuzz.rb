e5cb7cfa069829b49549d657fb8f23383eaa717d
hiromisugie <lozz.gggg@gmail.com>
2021-12-16 05:07:40 +0900

    fizzbuzz問題の再提出をいたします（12/16）

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,11 @@
-a = 0 # 0からスタート
-while a < 20 # 1から20までの数字を書く
-  a += 1 # 「a = a + 1」の省略形
-  unless a % 3 == 0 || a % 5 == 0 # 3の倍数でも5の倍数でもないの時は
-    puts a
-  end  
-  if a % 3 == 0 && a % 5 != 0 # 3の倍数かつ、5の倍数でない時は
-    puts "Fizz"
-  end
-  if a % 5 == 0 && a % 3 != 0 # 5の倍数かつ、3の倍数でない時は
-    puts "Buzz"
-  end
-  if a % 3 == 0 && a % 5 == 0 # 3の倍数かつ、5の倍数の時は
+(1..20).each do |a|
+  if a % 3 == 0 && a % 5 == 0
     puts "FizzBuzz"
+  elsif a % 3 == 0
+    puts "Fizz"
+  elsif a % 5 == 0
+    puts "Buzz"
+  else
+    puts a
   end
 end
