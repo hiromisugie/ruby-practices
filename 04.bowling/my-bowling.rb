846a9b4982496a947215dedefb4fe018089e38db
hiromisugie <lozz.gggg@gmail.com>
2021-12-30 07:05:57 +0900

    rubocopを通していなかったので、通したファイルを再度提出いたします。

--- 04.bowling/my-bowling.rb
+++ 04.bowling/my-bowling.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split( ',')
+scores = score.split(',')
 shots = []
 scores.each do |shot|
   if shot == 'X'
@@ -19,18 +20,19 @@ end
 
 point = 0
 frames.each_with_index do |frame, n|
-  if n == 9 || n == 10 || n == 11
-    point += frame.sum
-  elsif frames[n][0] == 10
-    if frames[n + 1][0] == 10
-      point += frame.sum + frames[n + 1][0] + frames[n + 2][0]
+  point +=
+    if [9, 10, 11].include?(n)
+      frame.sum
+    elsif frames[n][0] == 10
+      if frames[n + 1][0] == 10
+        frame.sum + frames[n + 1][0] + frames[n + 2][0]
+      else
+        frame.sum + frames[n + 1][0] + frames[n + 1][1]
+      end
+    elsif frame.sum == 10
+      frame.sum + frames[n + 1][0]
     else
-      point += frame.sum + frames[n + 1][0] + frames[n + 1][1]
+      frame.sum
     end
-  elsif frame.sum == 10
-    point += frame.sum + frames[n + 1][0]
-  else
-    point += frame.sum
-  end
 end
 puts point
