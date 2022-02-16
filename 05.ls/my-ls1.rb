bbb3a9206426c88c4f9d2c42b49023f13a28dc60
hiromisugie <lozz.gggg@gmail.com>
2022-02-17 06:20:28 +0900

    いただいたレビューをもとにlsコマンド1を修正しました。

--- 05.ls/my-ls1.rb
+++ 05.ls/my-ls1.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
-number_of_row = 3
-files = Dir.glob('*').sort
-number_of_elements_per_row = (files.length.to_f / number_of_row).ceil
-@files_table =
-  if files == []
-    []
-  else
-    files.each_slice(number_of_elements_per_row).to_a
-  end
+NUMBER_OF_COLUMN = 3
 
-def adjust_table
-  max_size = @files_table.map(&:size).max
-  @files_table.map! do |n|
-    n.values_at(0...max_size)
-  end
-  @files_table.each do |n|
-    n.map! { |x| x.nil? ? '' : x }
-  end
-end
-
-def build_space
-  @files_table.map do |n|
-    maximum_number_of_characters = n.max_by(&:length)
-    maximum_number_of_characters.length + 4
-  end
-end
-
-def display_ls
+def main
   display_table = adjust_table.transpose
   display_table.each do |column|
-    column.each_with_index do |n, number|
-      print n.ljust(build_space[number])
+    column.each_with_index do |cell, number|
+      print cell.ljust(build_space[number])
     end
     print "\n"
   end
 end
 
-display_ls
+def adjust_table
+  max_size = original_table.map(&:size).max
+  original_table.map! do |row|
+    row.values_at(0...max_size)
+  end
+  original_table.each do |row|
+    row.map! { |x| x.nil? ? '' : x }
+  end
+end
+
+def build_space
+  original_table.map do |row|
+    max_number_of_characters = row.max_by(&:size)
+    max_number_of_characters.size + 4
+  end
+end
+
+def original_table
+  files = Dir.glob('*').sort
+  elements_per_column = (files.size.to_f / NUMBER_OF_COLUMN).ceil
+  if files == []
+    []
+  else
+    files.each_slice(elements_per_column).to_a
+  end
+end
+
+main
