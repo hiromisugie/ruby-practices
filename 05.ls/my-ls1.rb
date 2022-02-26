46dc70f7f80c3682943470fda24ff2226c3d403a
hiromisugie <lozz.gggg@gmail.com>
2022-02-27 06:54:41 +0900

    lsコマンド1の提出ファイルを再修正しました。

--- 05.ls/my-ls1.rb
+++ 05.ls/my-ls1.rb
@@ -1,31 +1,16 @@
 # frozen_string_literal: true
 
 NUMBER_OF_COLUMN = 3
+NUMBER_OF_SPACE = 4
 
 def main
-  display_table = adjust_table.transpose
+  original_table_result = original_table
+  display_table = adjust_table(original_table_result).transpose
   display_table.each do |column|
     column.each_with_index do |cell, number|
-      print cell.ljust(build_space[number])
+      print cell.ljust(build_space(original_table_result)[number])
     end
     print "\n"
-  end
-end
-
-def adjust_table
-  max_size = original_table.map(&:size).max
-  original_table.map! do |row|
-    row.values_at(0...max_size)
-  end
-  original_table.each do |row|
-    row.map! { |x| x.nil? ? '' : x }
-  end
-end
-
-def build_space
-  original_table.map do |row|
-    max_number_of_characters = row.max_by(&:size)
-    max_number_of_characters.size + 4
   end
 end
 
@@ -36,6 +21,23 @@ def original_table
     []
   else
     files.each_slice(elements_per_column).to_a
+  end
+end
+
+def adjust_table(table)
+  max_size = table.map(&:size).max
+  filled_table = table.map do |row|
+    row.values_at(0...max_size)
+  end
+  filled_table.each do |row|
+    row.map! { |x| x.nil? ? '' : x }
+  end
+end
+
+def build_space(table)
+  table.map do |row|
+    max_number_of_characters = row.max_by(&:size)
+    max_number_of_characters.size + NUMBER_OF_SPACE
   end
 end
 
