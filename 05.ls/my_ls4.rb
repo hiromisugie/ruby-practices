8c9619523c52958ba69a8a4e45bda2457ad61886
hiromisugie <lozz.gggg@gmail.com>
2022-03-30 06:22:16 +0900

    lsコマンド4用のファイルを作成

---
+++ 05.ls/my_ls4.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+NUMBER_OF_COLUMN = 3
+NUMBER_OF_SPACE = 4
+
+def main
+  columns = make_columns
+  characters_per_column = count_characters_per_column(columns)
+  rows = align_number_of_files_in_column(columns).transpose
+  rows.each do |row|
+    row_with_margin = row.map.with_index do |file_or_folder, number|
+      file_or_folder.to_s.ljust(characters_per_column[number])
+    end
+    puts row_with_margin.join
+  end
+end
+
+def make_columns
+  options = ARGV.getopts('r')
+  files =
+    if options['r']
+      Dir.glob('*').sort.reverse
+    else
+      Dir.glob('*').sort
+    end
+
+  return [] if files.empty?
+
+  elements_per_column = (files.size.to_f / NUMBER_OF_COLUMN).ceil
+  files.each_slice(elements_per_column).to_a
+end
+
+def align_number_of_files_in_column(columns)
+  max_size = columns.map(&:size).max
+  columns.map do |column|
+    column.values_at(0...max_size)
+  end
+end
+
+def count_characters_per_column(columns)
+  columns.map do |column|
+    max_number_of_characters = column.max_by(&:size)
+    max_number_of_characters.size + NUMBER_OF_SPACE
+  end
+end
+
+main
