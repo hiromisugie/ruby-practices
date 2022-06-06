202a4fd1f1ce5bd2d32f341c2e4998f36f1116db
hiromisugie <lozz.gggg@gmail.com>
2022-06-07 05:41:56 +0900

    コードの重複を解決、standard_inputの分岐も削除、それらに伴う各所の調整

--- 06.wc/my_wc.rb
+++ 06.wc/my_wc.rb
@@ -5,61 +5,41 @@ require 'optparse'
 def main
   options = ARGV.getopts('l')
   contents = read_contents
-  lines = count_lines(contents)
-  words = count_words(contents)
-  bytes = count_bytes(contents)
-
-  if standard_input?
-    display_standard_input(options, lines, words, bytes)
-  else
-    display_files_input(options, contents, lines, words, bytes)
-  end
+  display_lines_words_bytes(options, contents)
 end
 
-def display_standard_input(options, lines, words, bytes)
-  puts adjust_lines_words_bytes(lines, words, bytes, '', options)
-end
-
-def display_files_input(options, contents, lines, words, bytes)
-  contents.zip(ARGV).each do |content, file|
-    each_lines = count_lines(content)
-    each_words = count_words(content)
-    each_bytes = count_bytes(content)
+def display_lines_words_bytes(options, contents)
+  total_lines = 0
+  total_words = 0
+  total_bytes = 0
+  contents.each do |content|
+    each_lines = content.values.flatten.count
+    total_lines += each_lines
+    each_words = content.values.join.split(/\s+/).count
+    total_words += each_words
+    each_bytes = content.values.join.length
+    total_bytes += each_bytes
+    file = content.keys.join
     puts adjust_lines_words_bytes(each_lines, each_words, each_bytes, file, options)
   end
 
   return if contents.size == 1
-  puts adjust_lines_words_bytes(lines, words, bytes, 'total', options)
+
+  puts adjust_lines_words_bytes(total_lines, total_words, total_bytes, 'total', options)
 end
 
 def adjust_lines_words_bytes(lines, words, bytes, file_or_total, options)
-  options['l'] ? "#{lines.to_s.rjust(8)}" + " #{file_or_total}" : "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}" + " #{file_or_total}"
-end
-
-def count_lines(contents)
-  contents.flatten.count
-end
-
-def count_words(contents)
-  contents.join.split(/\s+/).count
-end
-
-def count_bytes(contents)
-  contents.join.length
+  options['l'] ? lines.to_s.rjust(8) + " #{file_or_total}" : "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}" + " #{file_or_total}"
 end
 
 def read_contents
-  if standard_input?
-    readlines
+  if ARGV.empty?
+    [{ '' => readlines }]
   else
     ARGV.map do |file|
-      File.open(file, &:readlines)
+      { file => File.open(file, &:readlines) }
     end
   end
-end
-
-def standard_input?
-  ARGV.empty?
 end
 
 main
