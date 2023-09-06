c43ddb126d3e9699241143385e8008eec85b64a3
hiromisugie <lozz.gggg@gmail.com>
2023-09-07 05:33:19 +0900

    shotクラス、frameクラスの中身を記述

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,2 +1,18 @@
+require_relative 'shot'
+
 class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(first_pins, second_pins, third_pins = nil)
+    @first_shot = Shot.new(first_pins)
+    @second_shot = Shot.new(second_pins)
+    @third_shot = Shot.new(third_pins)
+  end
+
+  def score
+    first_shot.score + second_shot.score + third_shot.score
+  end
 end
+
+frame = Frame.new('1', '9')
+frame.score
--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,2 +1,16 @@
 class Shot
+  attr_reader :pins
+
+  def initialize(pins)
+    @pins = pins
+  end
+
+  def score
+    return 10 if pins == 'X'
+    pins.to_i
+  end
 end
+
+shot = Shot.new('X')
+shot.pins
+shot.score
