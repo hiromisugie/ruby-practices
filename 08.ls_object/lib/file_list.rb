8cb437fc29bc047f70fa80606a15fedf6654c8b8
hiromisugie <lozz.gggg@gmail.com>
2023-12-12 05:06:04 +0900

    privateメソッドをinitializeの中に直接記述

--- 08.ls_object/lib/file_list.rb
+++ 08.ls_object/lib/file_list.rb
@@ -4,13 +4,7 @@ class FileList
   attr_reader :file_names
 
   def initialize(options)
-    @file_names = file_list(options)
-  end
-
-  private
-
-  def file_list(options)
     files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
-    options['r'] ? files.reverse : files
+    @file_names = options['r'] ? files.reverse : files
   end
 end
